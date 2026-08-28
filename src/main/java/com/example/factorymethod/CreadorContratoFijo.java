567662375eb0961bacac98b1cad95cd934680d6b
Mauricio Ortiz Ochoa <33659629+mortizo@users.noreply.github.com>
2026-08-27 22:07:06 -0500

    feat: add CreadorContratoFijo.java

---
+++ src/main/java/com/example/factorymethod/CreadorContratoFijo.java
@@ -0,0 +1,21 @@
+package com.example.factorymethod;
+
+public final class CreadorContratoFijo extends CreadorContrato {
+    private final double salarioMensual;
+    private final double bonoDesempeno;
+    private final double horasExtra;
+    private final double tarifaHoraExtra;
+
+    public CreadorContratoFijo(double salarioMensual, double bonoDesempeno,
+                               double horasExtra, double tarifaHoraExtra) {
+        this.salarioMensual = salarioMensual;
+        this.bonoDesempeno = bonoDesempeno;
+        this.horasExtra = horasExtra;
+        this.tarifaHoraExtra = tarifaHoraExtra;
+    }
+
+    @Override
+    public Contrato crearContrato() {
+        return new ContratoFijo(salarioMensual, bonoDesempeno, horasExtra, tarifaHoraExtra);
+    }
+}
