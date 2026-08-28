ca03f6559a7d60147c27058ceb397750bd36ceac
morti <morti@CR2mortizo>
2026-08-27 21:23:58 -0500

    Inicio de proyecto

---
+++ pds-codex-factory-method-demo/src/main/java/com/mycompany/pds/codex/factory/method/demo/PdsCodexFactoryMethodDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pds.codex.factory.method.demo;
+
+/**
+ *
+ * @author morti
+ */
+public class PdsCodexFactoryMethodDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
